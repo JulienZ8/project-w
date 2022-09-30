af85977424df131c5b601af43ed44281fc224f95
JulienZ8 <julien.zermatten1@gmail.com>
2022-09-30 15:59:31 +0200

    caeser_cipher_init
    created branch
    created url
    created html

--- wapiti/cryptography/urls.py
+++ wapiti/cryptography/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('caesar/', views.caesar_cipher, name='caesar')
 ]
--- wapiti/cryptography/views.py
+++ wapiti/cryptography/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the cryptography index.")
+
+
+def caesar_cipher(request):
+    key = 4
+    text = 'salut'
+    return render(request, 'caesar.html', {'key': key, 'text': text})
