074f0b5db8fc0a5d58689c20cfd31f179b31bc68
yann <yann.cla@gmail.com>
2022-10-01 15:44:29 +0200

    Update views.py

--- wapiti/cryptography/views.py
+++ wapiti/cryptography/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+import string
 
 
 def index(request):
@@ -10,4 +11,9 @@ def index(request):
 def caesar_cipher(request):
     key = 4
     text = 'salut'
+    
+    # position dans l'alphabet
+    for x in text:
+        print(string.ascii_lowercase.index(x) + 1)
+
     return render(request, 'caesar.html', {'key': key, 'text': text})
