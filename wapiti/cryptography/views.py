b2d541ba8f1c6612cbe1a3c84a2f4f0ea390ef5f
JulienZ8 <julien.zermatten1@gmail.com>
2022-10-12 11:17:13 +0200

    Update views.py
    removed commented function

--- wapiti/cryptography/views.py
+++ wapiti/cryptography/views.py
@@ -8,24 +8,6 @@ import string
 def index(request):
     return HttpResponse("Hello, world. You're at the cryptography index.")
 
-#def caesar_cipher(request):
-
-#    form = CaesarCipherForm()
-#    key = 158
-#    key_mod = 158%26
-#    text = 'salut'
-#    new_text= ""
-#    print("hello")
-#    print(key, text)
-    
-    # position dans l'alphabet
-#    for i in range(len(text)):
-#        letter = text[i]
-#        new_letter = string.ascii_lowercase[string.ascii_lowercase.index(letter)+key_mod]
-#        new_text = new_text + new_letter
-
-#    return render(request, 'caesar.html', {'key': key, 'text': text, 'new_text': new_text, 'form': form})
-
 def cipher_form(request):
 
     new_text = ""
