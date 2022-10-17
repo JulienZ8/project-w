54392bcc627ac51097339c947ec4de7517d7a07a
JulienZ8 <julien.zermatten1@gmail.com>
2022-10-17 15:58:56 +0200

    Update tests.py
    created unit tests for caesar cipher function

--- wapiti/cryptography/tests.py
+++ wapiti/cryptography/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+import unittest
+from. import views
 
 # Create your tests here.
+class TestCaesarCipher(unittest.TestCase):
+
+    def test_caesar_cipher_27(self):
+        result = views.caesar_cipher(key=27, text="abc")
+        expected_result = "bcd"
+        self.assertEqual(result, expected_result)
+
+
+    def test_caesar_cipher_53(self):
+        result = views.caesar_cipher(key=53, text="abc")
+        expected_result = "bcd"
+        self.assertEqual(result, expected_result)
+
+    def test_caesar_cipher_special_char(self):
+        result = views.caesar_cipher(key=1, text="Salut, Yann clâvien!")
+        expected_result = "Tbmvu, Zboo dmâwjfo!"
+        self.assertEqual(result, expected_result)
