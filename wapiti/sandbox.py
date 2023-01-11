f8f9f856518d290303fa3e5a01a17e1cc04cd27f
JulienZ8 <julien.zermatten1@gmail.com>
2023-01-11 11:23:40 +0100

    created sandbox.py
    -to test out try/except/else
    -generates errormessage.txt

---
+++ wapiti/sandbox.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+def division(nominator: int, denominator: int) -> float:
+
+    try:
+        result = nominator/denominator
+    except ZeroDivisionError:
+        print('La division par zéro n''est pas possible')
+    except Exception as e:
+        f = open('errormessage.txt', 'a')
+        date_time = datetime.datetime.now()
+        f.write(f'\n date and time: {date_time} \n Error message: \n {e} \n nominator: {nominator}, denominator: {denominator} \n')
+        f.close()
+    else:
+        return result
+
+
+result = division(12, 0)
+
+print(result)
